65c92e4ca6afbec8d16a67a7ef8818196a7ef8f3
Nicholas Wojtalewicz <nicholaswojt@outlook.com>
2021-09-23 18:46:22 -0600

    Added comments

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 
 const db = require("../models");
-
+//Get Route for one workout
 router.get("/api/workouts", async (req, res) => {
   db.Workout.aggregate([
     {
